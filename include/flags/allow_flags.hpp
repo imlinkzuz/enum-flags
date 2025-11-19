7d7d46f8929494be3846d2e7a9383bdfc6ea366e
Linkzuz <imlinkzuz@gmail.com>
2025-11-19 22:29:27 +0800

    Refactor allow_flags.hpp: streamline macro definitions and simplify is_flags structure

--- include/flags/allow_flags.hpp
+++ include/flags/allow_flags.hpp
@@ -4,21 +4,20 @@
 
 #include <type_traits>
 
+#define ALLOW_FLAGS_FOR_ENUM(Name) inline void enableEnumFlags(Name) {}
+#define ALLOW_FLAGS_FOR_ENUM_IN_CLASS(Name) friend inline void enableEnumFlags(Name) {}
 
 namespace flags {
 
-
-template <class E, class Enabler = void> struct is_flags
-: public std::false_type {};
+template <typename T, typename = void>
+struct is_flags : std::false_type {};
+template <typename T>
+struct is_flags<T, decltype(enableEnumFlags(T{}))> : std::true_type {};
 
 
 } // namespace flags
 
 
-#define ALLOW_FLAGS_FOR_ENUM(name) \
-namespace flags { \
-template <> struct is_flags< name > : std::true_type {}; \
-}
 
 
 #endif // ENUM_CLASS_ALLOW_FLAGS_HPP
